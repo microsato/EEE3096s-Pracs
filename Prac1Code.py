9d0820658ec128c0897436a1393a309911faf88f
microsato <53040203+microsato@users.noreply.github.com>
2019-08-04 00:13:54 +0200

    Update Prac1Code.py

--- Prac1Code.py
+++ Prac1Code.py
@@ -15,50 +15,50 @@ import RPi.GPIO as GPIO
 from itertools import product
 
 #constants and global variables
-counter = 0
+counter = 0 #global counter
 
 #setup GPIO pins
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17,GPIO.OUT)
-GPIO.setup(27,GPIO.OUT)
-GPIO.setup(22,GPIO.OUT)
+GPIO.setwarnings(False) 
+GPIO.setmode(GPIO.BCM) #use BCM numbering of pins
+GPIO.setup(17,GPIO.OUT) #LED 1
+GPIO.setup(27,GPIO.OUT) #LED 2
+GPIO.setup(22,GPIO.OUT) #LED 3
 
-GPIO.setup(6,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(5,GPIO.IN,pull_up_down=GPIO.PUD_UP) #sw pin5  dec
-GPIO.remove_event_detect(6)
-GPIO.remove_event_detect(5)
+GPIO.setup(6,GPIO.IN, pull_up_down=GPIO.PUD_UP) #SW1 (inc) - internal pull up res
+GPIO.setup(5,GPIO.IN,pull_up_down=GPIO.PUD_UP) #SW2 (dec) - internal pull up res 
+GPIO.remove_event_detect(6) #remove past interrupts sw1
+GPIO.remove_event_detect(5) #remove past interrupts sw2
 
-LED_Pins = [17,27,22]
+LED_Pins = [17,27,22] #array of LED pins
 
-comb = list(product([1,0],repeat=3))
+comb = list(product([1,0],repeat=3)) #create binary list (1,1,1) -> (0,0,0) 
 
-arr=comb[::-1]
+arr=comb[::-1] #reverse comb list so that (0,0,0) -> (1,1,1) 
 
-def main():
-        GPIO.setwarnings(False)
-def incriment(channel):
-        global counter
-        if (counter == 7):
-                counter = 0 #reset the counter
+def main(): #continously runs
+        GPIO.setwarnings(False) 
+def incriment(channel): #interrupt sw1
+        global counter 
+        if (counter == 7): #if LEDs all on-counter full
+                counter = 0 #reset the counter 
         else:
                 counter=counter+1 #incriment counter by 1
-        LEDs_On()
+        LEDs_On() #display current counter value on LEDs
 
 def decriment(channel):
         global counter
-        if (counter == 0):
-                counter=7
+        if (counter == 0): #if counter empty
+                counter=7 #loop around counter - full 
         else:
                 counter=counter-1 #decriment counter by 1
-        LEDs_On()
+        LEDs_On() #display current counter value on LEDs
 
 def LEDs_On():
-        GPIO.output(LED_Pins,arr[counter])
+        GPIO.output(LED_Pins,arr[counter]) #correspond LED pin to counter pos in binary array
 
 
-GPIO.add_event_detect(6,GPIO.FALLING,callback=incriment,bouncetime=250)
-GPIO.add_event_detect(5,GPIO.FALLING,callback=decriment,bouncetime=250)
+GPIO.add_event_detect(6,GPIO.FALLING,callback=incriment,bouncetime=250) #sw1 interrupt- falling edge, incriments counter , debounce time 250ms
+GPIO.add_event_detect(5,GPIO.FALLING,callback=decriment,bouncetime=250)#sw2 interrupt- falling edge, decriments counter , debounce time 250ms
 
 
 # Only run the functions if
