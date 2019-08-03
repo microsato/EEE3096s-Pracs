7afd17a033e5ba2e2df9eeb884bbbf0e953c8d2c
Mic Rosato <RSTMIC005@myuct.ac.za>
2019-08-03 22:57:27 +0100

    LED Binary Counter with Incriment&Decriment Buttons

--- Prac1Code.py
+++ Prac1Code.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """
 Python Practical Template
@@ -11,26 +12,53 @@ Date: 26/07/19
 
 # import Relevant Librares
 import RPi.GPIO as GPIO
+from itertools import product
+
 #constants and global variables
+counter = 0
 
 #setup GPIO pins
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17,GPIO.OUT)
+GPIO.setup(27,GPIO.OUT)
+GPIO.setup(22,GPIO.OUT)
 
-GPIO.setup(5,GPIO.IN,pull_up_down=GPIO.PUD_UP) #set button as input, pull up resistors - when pressed goes low
-GPIO.remove_event_detect(5) #initially remove all previous events
+GPIO.setup(6,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(5,GPIO.IN,pull_up_down=GPIO.PUD_UP) #sw pin5  dec
+GPIO.remove_event_detect(6)
+GPIO.remove_event_detect(5)
 
+LED_Pins = [17,27,22]
+
+comb = list(product([1,0],repeat=3))
+
+arr=comb[::-1]
 
 def main():
         GPIO.setwarnings(False)
+def incriment(channel):
+        global counter
+        if (counter == 7):
+                counter = 0 #reset the counter
+        else:
+                counter=counter+1 #incriment counter by 1
+        LEDs_On()
 
-def LightUp(channel):
-        GPIO.output(17,1) #turn on LED
-	time.sleep(3) #wait for 3s
-	GPIO.output(17,0) #turn LED off
+def decriment(channel):
+        global counter
+        if (counter == 0):
+                counter=7
+        else:
+                counter=counter-1 #decriment counter by 1
+        LEDs_On()
 
-GPIO.add_event_detect(5,GPIO.FALLING,callback=LightUp,bouncetime=250) #interrupt on falling edge (when pressed) + debounce time 250ms 
+def LEDs_On():
+        GPIO.output(LED_Pins,arr[counter])
+
+
+GPIO.add_event_detect(6,GPIO.FALLING,callback=incriment,bouncetime=250)
+GPIO.add_event_detect(5,GPIO.FALLING,callback=decriment,bouncetime=250)
 
 
 # Only run the functions if
